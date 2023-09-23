361c2aae99056e5edfcb3ee8de1fac98b9248e62
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-09-23 13:51:31 +0200

    pizzas tuto 5

--- tutorials/pizzas/routes/pizzas.js
+++ tutorials/pizzas/routes/pizzas.js
@@ -30,7 +30,7 @@ let MENU = [
 ];
 
 
-
+// GET /pizzas/id
 
 router.get('/:id', (req, res, next) =>{
     console.log(`GET /pizzas/${req.params.id}`);//req.params.id permes de lire le parametre 
@@ -63,7 +63,29 @@ router.get('/', (req, res, next) => {
   });
 
 
-
+// Create a pizza to be added to the menu.
+router.post('/', (req, res) => {
+    const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+    const content = req?.body?.content?.length !== 0 ? req.body.content : undefined;
+  
+    console.log('POST /pizzas');
+  
+    if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+  
+    const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
+    const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+    const nextId = lastId + 1;
+  
+    const newPizza = {
+      id: nextId,
+      title: title,
+      content: content,
+    };
+  
+    MENU.push(newPizza);
+  
+    res.json(newPizza);
+  });
 
 
 module.exports = router;
