e3c2f8a2c2fecf421ec5adbb8b7a69dc1701412e
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-10-02 08:06:10 +0200

    tuto suite

--- tutorials/pizzas/app.js
+++ tutorials/pizzas/app.js
@@ -3,8 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var pizzaRouter = require('./routes/pizzas');
 
 var app = express();
@@ -20,8 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/pizzas', pizzaRouter);
 
 module.exports = app;
--- tutorials/pizzas/routes/pizzas.js
+++ tutorials/pizzas/routes/pizzas.js
@@ -1,91 +1,238 @@
 var express = require('express');
+
+const { serialize, parse } = require('../utils/json');
+
 var router = express.Router();
 
-let MENU = [
-    {
-        id: 1,
-        title: 'Alpage',
-        content: 'contenu de la pizza'
-    },
-    {
-        id: 2,
-        title: 'Alpage1',
-        content: 'contenu de la pizza'
-    },
-    {
-        id: 3,
-        title: 'Alpage2',
-        content: 'contenu de la pizza'
-    },
-    {
-        id: 4,
-        title: 'Alpage3',
-        content: 'contenu de la pizza'
-    },
-    {
-        id: 5,
-        title: 'Alpage4',
-        content: 'contenu de la pizza'
-    },
+
+const jsonDbPath = __dirname + '/../data/pizzas.json';
+
+
+const MENU = [
+
+  {
+
+    id: 1,
+
+    title: '4 fromages',
+
+    content: 'Gruyère, Sérac, Appenzel, Gorgonzola, Tomates',
+
+  },
+
+  {
+
+    id: 2,
+
+    title: 'Vegan',
+
+    content: 'Tomates, Courgettes, Oignons, Aubergines, Poivrons',
+
+  },
+
+  {
+
+    id: 3,
+
+    title: 'Vegetarian',
+
+    content: 'Mozarella, Tomates, Oignons, Poivrons, Champignons, Olives',
+
+  },
+
+  {
+
+    id: 4,
+
+    title: 'Alpage',
+
+    content: 'Gruyère, Mozarella, Lardons, Tomates',
+
+  },
+
+  {
+
+    id: 5,
+
+    title: 'Diable',
+
+    content: 'Tomates, Mozarella, Chorizo piquant, Jalapenos',
+
+  },
+
 ];
 
 
-// GET /pizzas/id
-
-router.get('/:id', (req, res, next) =>{
-    console.log(`GET /pizzas/${req.params.id}`);//req.params.id permes de lire le parametre 
-
-    const indexOfPizzaFound = MENU.findIndex((pizza) => pizza.id == req.params.id); //iter sur notre tableau et retourn l'index de la pizza (id)
-
-    if(indexOfPizzaFound < 0) return res.sendStatus(404);
-
-    res.json(MENU[indexOfPizzaFound]);
-})
-
-
 /* Read all the pizzas from the menu
+
    GET /pizzas?order=title : ascending order by title
+
    GET /pizzas?order=-title : descending order by title
+
 */
+
 router.get('/', (req, res, next) => {
-    const orderByTitle =
-      req?.query?.order?.includes('title') // si il y a query et order et contiend titre alors orderByTitle = req.query.order sinon undefined
-        ? req.query.order
-        : undefined;
-    let orderedMenu;
-    console.log(`order by ${orderByTitle ?? 'not requested'}`); // ?? si null ou undefined 'not requested' sinon orderByTitle
-    if (orderByTitle)
-      orderedMenu = [...MENU].sort((a, b) => a.title.localeCompare(b.title));//shadowcopy
-    if (orderByTitle === '-title') orderedMenu = orderedMenu.reverse();
+
+  const orderByTitle =
+
+    req?.query?.order?.includes('title') ? req.query.order : undefined;
+
+  let orderedMenu;
+
+  console.log(`order by ${orderByTitle ?? 'not requested'}`);
+
+
+  const pizzas = parse(jsonDbPath, MENU);
+
   
-    console.log('GET /pizzas');
-    res.json(orderedMenu ?? MENU);
-  });
+
+  if (orderByTitle) orderedMenu = [...pizzas].sort((a, b) => a.title.localeCompare(b.title));
+
+  if (orderByTitle === '-title') orderedMenu = orderedMenu.reverse();
+
+
+  console.log('GET /pizzas');
+
+  return res.json(orderedMenu ?? pizzas);
+
+});
+
+
+// Read the pizza identified by an id in the menu
+
+router.get('/:id', (req, res) => {
+
+  console.log(`GET /pizzas/${req.params.id}`);
+
+
+  const pizzas = parse(jsonDbPath, MENU);
+
+
+  const indexOfPizzaFound = pizzas.findIndex(pizza => pizza.id == req.params.id);
+
+
+  if (indexOfPizzaFound < 0) return res.sendStatus(404);
+
+
+  return res.json(pizzas[indexOfPizzaFound]);
+
+});
 
 
 // Create a pizza to be added to the menu.
+
 router.post('/', (req, res) => {
-    const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
-    const content = req?.body?.content?.length !== 0 ? req.body.content : undefined;
-  
-    console.log('POST /pizzas');
-  
-    if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
-  
-    const lastItemIndex = MENU?.length !== 0 ? MENU.length - 1 : undefined;
-    const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
-    const nextId = lastId + 1;
-  
-    const newPizza = {
-      id: nextId,
-      title: title,
-      content: content,
-    };
-  
-    MENU.push(newPizza);
-  
-    res.json(newPizza);
-  });
+
+  const title = req?.body?.title?.length !== 0 ? req.body.title : undefined;
+
+  const content = req?.body?.content?.length !== 0 ? req.body.content : undefined;
+
+
+  console.log('POST /pizzas');
+
+
+  if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+
+
+  const pizzas = parse(jsonDbPath, MENU);
+
+  const lastItemIndex = pizzas?.length !== 0 ? pizzas.length - 1 : undefined;
+
+  const lastId = lastItemIndex !== undefined ? pizzas[lastItemIndex]?.id : 0;
+
+  const nextId = lastId + 1;
+
+
+  const newPizza = {
+
+    id: nextId,
+
+    title: title,
+
+    content: content,
+
+  };
+
+
+  pizzas.push(newPizza);
+
+
+  serialize(jsonDbPath, pizzas);
+
+
+  return res.json(newPizza);
+
+});
+
+
+// Delete a pizza from the menu based on its id
+
+router.delete('/:id', (req, res) => {
+
+  console.log(`DELETE /pizzas/${req.params.id}`);
+
+
+  const pizzas = parse(jsonDbPath, MENU);
+
+
+  const foundIndex = pizzas.findIndex(pizza => pizza.id == req.params.id);
+
+
+  if (foundIndex < 0) return res.sendStatus(404);
+
+
+  const itemsRemovedFromMenu = pizzas.splice(foundIndex, 1);
+
+  const itemRemoved = itemsRemovedFromMenu[0];
+
+
+  serialize(jsonDbPath, pizzas);
+
+
+  return res.json(itemRemoved);
+
+});
+
+
+// Update a pizza based on its id and new values for its parameters
+
+router.patch('/:id', (req, res) => {
+
+  console.log(`PATCH /pizzas/${req.params.id}`);
+
+
+  const title = req?.body?.title;
+
+  const content = req?.body?.content;
+
+
+  console.log('POST /pizzas');
+
+
+  if ((!title && !content) || title?.length === 0 || content?.length === 0) return res.sendStatus(400);
+
+
+  const pizzas = parse(jsonDbPath, MENU);
+
+
+  const foundIndex = pizzas.findIndex(pizza => pizza.id == req.params.id);
+
+
+  if (foundIndex < 0) return res.sendStatus(404);
+
+
+  const updatedPizza = {...pizzas[foundIndex], ...req.body};
+
+
+  pizzas[foundIndex] = updatedPizza;
+
+
+  serialize(jsonDbPath, pizzas);
+
+
+  return res.json(updatedPizza);
+
+});
 
 
 module.exports = router;
