f3d79f9e3aa710299694a6741cf2e7c7c3402552
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-10-17 13:49:03 +0200

    update

--- tutorials/ihm/modern/src/index.js
+++ tutorials/ihm/modern/src/index.js
@@ -1,5 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'animate.css';
 
 const body = document.querySelector("body");
 
