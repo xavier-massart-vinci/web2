50307235307337bf6222dc14aca85368c53fd190
xavier-massart-vinci <xavier.massart@student.vinci.be>
2023-10-31 09:02:38 +0100

    2.8: dynamic HomePage

---
+++ projet/2.8/src/index.js
@@ -0,0 +1,34 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './stylesheets/main.css';
+
+import oppenheimer from './img/oppenheimer.png';
+import interstellar from './img/interstellar.png';
+
+
+
+
+const homePage = `
+<div class="container text-center">
+    <div class="row mb-4">
+        <div class="col">
+        <h3>myMovies</h3>
+
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12 col-lg-6">
+        <img class="img-thumbnail" src="${oppenheimer}" alt="Oppenheimer" />
+        <span>Oppenheimer</span>
+        </div>
+
+        <div class="col-12 col-lg-6">
+        <img class="img-thumbnail" src="${interstellar}" alt="Interstellar" />
+        <span>Interstellar</span>
+        </div>
+    </div>
+</div>`;
+
+const main = document.querySelector("main");
+
+main.innerHTML = homePage;
